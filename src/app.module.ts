9dc160303bc44c2c7837cd69d8476e6dc209209c
rahul chourasiya <rahul.chourasiya@rupyy.com>
2026-01-16 14:33:12 +0530

    adding database and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     // 10 requests per minute 
+    TypeOrmModule.forRoot({
+      type: 'mysql', 
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '3306', 10),
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'boilerplate_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     ThrottlerModule.forRoot([{
       ttl: 60000,
       limit: 10,
---
+++ src/database/seeds/init.seed.ts
@@ -0,0 +1,24 @@
+import { DataSource } from 'typeorm';
+
+
+const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST || 'localhost',
+  port: 3306,
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'boilerplate_db',
+  entities: [], 
+});
+
+const seed = async () => {
+  await AppDataSource.initialize();
+  console.log('Seeding started...');
+
+
+
+  console.log('Seeding completed!');
+  await AppDataSource.destroy();
+};
+
+seed();
