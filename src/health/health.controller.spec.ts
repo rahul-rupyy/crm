32a49b0e594e047e8aee6cd08fbfdc8278c31b04
rahul chourasiya <rahul.chourasiya@rupyy.com>
2026-01-27 15:23:37 +0530

    initilsed mocha chai

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -1,18 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
-
+import { HealthCheckService, TypeOrmHealthIndicator } from '@nestjs/terminus';
+import { expect } from 'chai';
 describe('HealthController', () => {
   let controller: HealthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HealthController],
+      providers: [
+        {
+          provide: HealthCheckService,
+          useValue: {
+            check: () => Promise.resolve({}),
+          },
+        },
+        {
+          provide: TypeOrmHealthIndicator,
+          useValue: {
+            pingCheck: () => Promise.resolve({}),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<HealthController>(HealthController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(controller).to.not.equal(undefined);
   });
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,13 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+  let app: INestApplication;
 
-  beforeEach(async () => {
+  before(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -16,10 +15,14 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  after(async () => {
+    await app.close();
+  });
+
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Hello World');
   });
 });
